770247c37e510b22078b36df6c56bcd4ba7755aa
Prathmesh <Prathmesh@gmail.com>
2026-06-01 13:38:14 +0530

    Initial commit with Day-1 exercises

---
+++ Desktop/Java Training/Day-1/Assignment_01.java
@@ -0,0 +1,13 @@
+// Write a Java program to print your batch name, roll number and college name on the console.
+
+public class Assignment_01 {
+    public static void main(String[] args) {
+        String batchName = "SD 01";    // example: batch number (use your actual batch)
+        int rollNumber = 1;   // example: roll number
+        String collegeName = "MIT ADT Univeersity,pune"; // example: college name
+
+        System.out.println("Batch Name: " + batchName);
+        System.out.println("Roll Number: " + rollNumber);
+        System.out.println("College Name: " + collegeName);
+    }
+}
